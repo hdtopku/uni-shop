cc2d091bd92f16dff99e956c190c67f3cbeb9c15
hdtopku <1450948930@qq.com>
2022-05-19 21:38:03 +0800

    自动描述：2022-05-19-21:38:02

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -10,7 +10,7 @@
       <u-radio-group v-model="radiovalue7" :borderBottom="true" placement="column" iconPlacement="right"
         @change="groupChange">
         <u-radio :customStyle="{marginBottom: '8px'}" v-for="(item, index) in radiolist7" :key="index"
-          :label="item.label" :name="item.name" @change="radioChange">
+          :label="item.label" :name="item.name">
         </u-radio>
       </u-radio-group>
       <u-alert v-show="showAlert" class="animate__animated animate__headShake" :title="alertTitle" :type="alertType"
@@ -29,10 +29,18 @@
         </u-collapse-item>
         <u-collapse-item title="2、慢打不开？">
           <text class="u-collapse-content">
-            网差慢，耐心等。或这3个方法都试试：换网、重启手机重来、或换其他iphone、ipad设备重来、或翻q
+            网差慢，耐心等。
+
+            这3个方法都试试：重启手机、换网重来
+            换其他iphone、ipad设备重来、或翻q
           </text>
         </u-collapse-item>
-        <u-collapse-item title="3、优惠期限？">
+        <u-collapse-item title="3、是否成功？">
+          <text class="u-collapse-content">
+            点开【系统设置头像订阅】：学生(1个月) 5元勾上，且没提示验证，即成功。
+          </text>
+        </u-collapse-item>
+        <u-collapse-item title="4、优惠期限？">
           <text class="u-collapse-content">
             苹果官方1-4年会对资格抽查
             如果抽到：音乐界面会提前一个月提示验证
@@ -40,25 +48,6 @@
 
             有客户360天说提示验证，以不到365天为由，要求客服免费加一年。
             特别提示：人工和升级链均需成本，希望客户能尊重客服的劳动。
-          </text>
-        </u-collapse-item>
-        <u-collapse-item title="4、是否成功？">
-          <text class="u-collapse-content">
-            点开【系统设置头像订阅】：学生(1个月) 5元勾上，且没提示验证，即成功。
-          </text>
-        </u-collapse-item>
-        <u-collapse-item title="5、日期没变？">
-          <text class="u-collapse-content">
-            您的方案是包月，非包年！
-            5元方案勾上，且没提示验证，就已成功！！
-
-            👉请勿反复问，谢谢支持！！！
-          </text>
-        </u-collapse-item>
-        <u-collapse-item title="6、如何有效和客服沟通？【必须先截图】">
-          <text class="u-collapse-content">
-            ✅ 必须点开【系统设置头像订阅】，先截图再提问。
-            ❌ 若未截图直接说：搞不定！客服将无法判断，因此必须截图给客服，才能精准解答！
           </text>
         </u-collapse-item>
       </u-collapse>
@@ -82,9 +71,13 @@
         <u-checkbox labelSize="18" size="25" label="我已认真阅读并知晓所有问题，可以开始验证！" :name="true">
         </u-checkbox>
       </u-checkbox-group>
-      <u-modal showCancelButton :closeOnClickOverlay="true" :show="showModal" title="如何有效和客服沟通？" cancelText="我再想想"
+      <u-modal showCancelButton :closeOnClickOverlay="true" :show="showModal" title="提问必须带上截图！" cancelText="我再想想"
         @close="showModal = false" @cancel="showModal = false" confirmColor="red" confirmText="继续！保证会截图"
         @confirm="confirm" :content='modalContent'>
+      </u-modal>
+      <u-modal showCancelButton :closeOnClickOverlay="true" :show="showRenewModal" title="验证完日期没变？" cancelText="懵了！再想想"
+        @close="showRenewModal = false" @cancel="showRenewModal = false" confirmColor="red" confirmText="继续！明白了"
+        @confirm="confirmRenew" :content='renewModalContent'>
       </u-modal>
     </view>
   </page>
@@ -96,9 +89,15 @@
       return {
         showAlert: true,
         currentStep: 0,
+        showRenewModal: false,
+        renewModalContent: '',
         showModal: false,
-        modalContent: `任何提问必须【系统设置头像订阅】截图给客服，再提问！
-        这是唯一有效提问凭证！`,
+        modalContent: `有疑问？
+        必须【系统设置头像订阅】截图给客服，再提问！这是唯一有效提问凭证！
+        
+        没疑问，很快很满意！
+        记得给小店好的评哦！
+        `,
         checkboxValue1: [false],
         alertType: 'error',
         alertTitle: '请正确选择您的情况，否则可能导致验证失败！',
@@ -130,8 +129,22 @@
     },
     onLoad(option) {
       uni.$u.checkAmEnv()
+      this.getYear()
     },
     methods: {
+      getYear() {
+        let date = new Date();
+        let thisYear = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
+        let nextYear = (date.getFullYear() + 1) + "-" + (date.getMonth() + 1) + "-" + date.getDate();
+        this.renewModalContent = `问：假设续期时间是：${thisYear}
+        验证完，为啥不是：${nextYear}
+        
+        答：您的方案是包月，非包年！
+        点开【系统设置头像订阅】，没有提示验证，就已成功续一年！
+        
+        👉 请勿反复问客服为啥没加1年，感谢理解！
+        `
+      },
       checkboxChange(val) {
         this.checkboxValue1 = val
       },
@@ -168,11 +181,6 @@
           case 3:
             this.alertType = 'success'
             this.alertTitle = `我保证已经处于【个人方案】中，且未取消、未过期，可以进入下一步！
-            
-            温馨提示：如何判断是否成功升级学生方案？
-            打开【系统设置头像订阅】，学生5元/月勾上即成功！
-            日期没变？是因为您的方案是包月，非包年！
-            升级过程中需人脸或指纹确认？但不会扣费，请爽快继续！
             `
             this.alertEffect = 'dark'
             this.nextDisabled = false
@@ -180,24 +188,20 @@
           case 4:
             this.alertType = 'success'
             this.alertTitle = `我保证已经处于【学生方案】中，且未取消、未过期，目前苹果提示我验证资格，可以下一步！
-            
-            温馨提示：如何判断是否成功续期？
-            打开【系统设置头像订阅】，没有提示验证，则此次续期成功！
-            日期没变？是因为您的方案是包月，非包年！
-            升级过程中需人脸或指纹确认，但不会扣费，请爽快继续！
 `
             this.alertEffect = 'dark'
             this.nextDisabled = false
             break
         }
       },
-      radioChange(n) {},
       clickNext() {
         if (this.currentStep === 0 && this.radiovalue7 < 3) {
           this.showAlert = false
           this.$nextTick(() => {
             this.showAlert = true
           })
+        } else if (this.currentStep === 0 && this.radiovalue7 == 4) {
+          this.showRenewModal = true
         } else {
           this.currentStep = ++this.currentStep % 2
         }
@@ -218,6 +222,10 @@
           'https://itunes.apple.com/studentSubscriptionOffers?app=music&ud_h=cEv3MQq6Aj8alkFkGwcFECset/pXKjxW4sOwjpMqLTGlRDLIgBehkWv7FMiolTRwZT1OspZE76LOzh70DftfFw==&ud_s=lu71Beg7pESvcKjG7JPTdQ==&ud_t=1629938295'
         )
         // #endif
+      },
+      confirmRenew() {
+        this.showRenewModal = false
+        this.currentStep = ++this.currentStep % 2
       }
     }
   }
