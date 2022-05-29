63897fc054e2a4db4f86f1e7cfc7c7b8901f4407
hdtopku <1450948930@qq.com>
2022-05-29 23:59:41 +0800

    自动描述：2022-05-29-23:59:41

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -29,7 +29,7 @@
         </u-collapse-item>
         <u-collapse-item title="2、是否成功？">
           <text class="u-collapse-content">
-            点开【系统设置头像订阅】，学生(1个月) 5元勾上
+            点开【系统设置头像订阅Apple Music】，学生(1个月) 5元勾上
             并且【验证大学生身份】消失即成功。
           </text>
         </u-collapse-item>
@@ -101,7 +101,7 @@
         renewModalContent: '',
         showModal: false,
         modalContent: `有疑问？
-        必须【系统设置头像订阅】截图给客服，再提问！这是唯一有效提问凭证！
+        必须【系统设置头像订阅Apple Music】截图给客服，再提问！这是唯一有效提问凭证！
         
         没疑问！很快很满意，好评！
         `,
