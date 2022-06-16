6cc55ca1aa5b174b17dffc3b4755923f670d30ca
hdtopku <1450948930@qq.com>
2022-06-16 19:26:05 +0800

    自动描述：2022-06-16-19:26:05

--- src/pages/am/refund.vue
+++ src/pages/am/refund.vue
@@ -1,19 +1,12 @@
 <template>
   <view style="font-size: 50upx; padding: 500upx 50upx 0;">
-    <u-alert fontSize="25" show-icon :title="title" type="success"></u-alert>
-    <u-divider textSize="20" text='相逢即缘，好聚好散！'></u-divider>
-    接下来，请前往订单处申请退款，<text style="color:red">退款原因：其他/协商一致</text>
+    <u-alert fontSize="25" title="退款已发起" description="相逢即缘，好聚好散！" type="error"></u-alert>
+    <u-gap></u-gap>
+    接下来，请前往订单申请退款，<text style="color:red">退款原因：其他/协商一致</text>
   </view>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        title: '退款已发起',
-      }
-    },
-  }
 </script>
 
 <style>
