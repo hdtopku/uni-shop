3af4f6f3196ada2c7dd0006f93e2c00e5fb0d131
hdtopku <1450948930@qq.com>
2022-06-16 19:34:26 +0800

    自动描述：2022-06-16-19:34:26

--- src/pages/am/refund.vue
+++ src/pages/am/refund.vue
@@ -2,7 +2,8 @@
   <view style="font-size: 50upx; padding: 500upx 50upx 0;">
     <u-alert fontSize="25" title="退款已发起" description="相逢即缘，好聚好散！" type="error"></u-alert>
     <u-gap></u-gap>
-    接下来，请前往订单申请退款，<text style="color:red">退款原因：其他/协商一致</text>
+    接下来，请前往订单申请退款，退款选择以下原因，可享受免人工审核，系统自动秒退：
+    <view style="color:red">未收到货，退款原因选择：其他/协商一致</view>
   </view>
 </template>
 
