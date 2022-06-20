cfc778cc3f5b6325e5f52fb6c7db5f570e86de60
hdtopku <1450948930@qq.com>
2022-06-20 19:10:27 +0800

    自动描述：2022-06-20-19:10:27

--- src/pages/am/components/StepTwo.vue
+++ src/pages/am/components/StepTwo.vue
@@ -11,8 +11,8 @@
       </u-collapse-item>
       <u-collapse-item title="2、慢打不开？">
         <text class="u-collapse-content">
-          Safari可能出现<text style="color:red;">加载慢、或无法验证</text>。
-          换<text style="color:red;">谷歌、夸克</text>等第三方刘览器可解决。
+          【注意】Safari可能<text style="color:red;">提示无法验证、白屏、卡死、没反应等</text>。
+          换<text style="color:red;">夸克、谷歌</text>等第三方刘览器可解决。
           <img src="https://article.biliimg.com/bfs/article/cd4a5f4e5b23241198f313f9cced30730ce80060.jpg"
             style="margin: 0 auto;display: block;width: 80%;" />
         </text>
