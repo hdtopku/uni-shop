9dae8e905eab1a2767d3a769c2dffa3b26b922c3
hdtopku <1450948930@qq.com>
2022-06-25 20:23:35 +0800

    自动描述：2022-06-25-20:23:35

--- src/pages/am/components/StepOne.vue
+++ src/pages/am/components/StepOne.vue
@@ -16,7 +16,7 @@
         v-if="radiovalue7 !==1" @click="clickNext" type="error" plain shape="circle">{{buttonText}}
       </u-button>
       <view v-else style="color:gray" v-show="this.radiovalue7 === 1" class="animate__animated animate__slideInRight">
-        若无免费不愿重开，<a href="javascript:;" @click="showRefund=true">申请退单终止验证</a>
+        若已过期不愿重开，<a href="javascript:;" @click="showRefund=true">申请退单终止验证</a>
       </view>
     </view>
     <u-modal @close="showExpireModal = false" :show="showExpireModal" confirmText="确定"
@@ -114,18 +114,18 @@
         switch (n) {
           case 1:
             this.alertType = 'error'
-            // this.alertTitle = `【步骤】
-            // 1、自己到音乐软件里，先开10元方案
-            // 2、再回来选择情况2，升级为5元方案
-
-            // 【温馨提醒】
-            // 过期用户：首月10元（不可退还），下月起5元！
-            // 已取消、新用户：免费开10元方案（若试用过，则无免费）`
             this.alertTitle = `【步骤】
-            1、过期用户到支付宝搜：Apple专区，领红包并免费开通10元方案。
-            已取消、新用户到音乐软件里：免费开10元方案。
-            
-            2、再回来选择情况2，升级为5元方案`
+            1、自己到音乐软件里，先开10元方案
+            2、再回来选择情况2，升级为5元方案
+
+            【温馨提醒】
+            过期用户：首月10元（不可退还），下月起5元！
+            已取消、新用户：免费开10元方案（若试用过，则无免费）`
+            // this.alertTitle = `【步骤】
+            // 1、过期用户到支付宝搜：Apple专区，领红包并免费开通10元方案。
+            // 已取消、新用户到音乐软件里：免费开10元方案。
+
+            // 2、再回来选择情况2，升级为5元方案`
             this.alertEffect = 'light'
             this.buttonText = '自己先去开个人方案，开完按情况2升级'
             break
