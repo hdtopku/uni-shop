8081013b0403c9c77a2942d14995fc8e64c832f6
hdtopku <1450948930@qq.com>
2022-08-18 14:16:52 +0800

    自动描述：2022-08-18-14:16:52

--- src/pages/am/components/StepOne.vue
+++ src/pages/am/components/StepOne.vue
@@ -19,10 +19,10 @@
         </u-button>
         <view v-show="this.radiovalue7 === 1" class="explain animate__animated animate__slideInRight animate__slow"
           style="font-size: 28upx;">因官方改版，
-          <view style="color:red;font-weight: bolder;font-size: 40upx;display: inline-block;"
+          <view style="color:red;font-weight: bolder;font-size: 38upx;display: inline-block;"
             class="animate__animated animate__shakeY animate__infinite">必须</view>
-          <span><span style="color:red">由个人转学生</span>。首月先开个人(家庭和声控不行)</span>，再选<span
-            style="color:red">2、订阅中</span>升级下月起5元！过期用户10元重开，若不愿重开，<a href="javascript:;"
+          <span><span style="color:red">由个人转学生</span>。首月先开个人(家庭和声控不行)</span>，再选下方<span
+            style="color:red;">2、订阅中</span>升级下月起5元！过期用户10元重开，若不愿重开，<a href="javascript:;"
             @click="showRefund=true">点此退单终止验证</a>
 
         </view>
