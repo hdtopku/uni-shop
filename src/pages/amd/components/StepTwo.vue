f93a31cc02f4da8f0124ed573f66afca532a6228
hdtopku <1450948930@qq.com>
2022-10-06 16:11:18 +0800

    自动描述：2022-10-06-16:11:18

--- src/pages/amd/components/StepTwo.vue
+++ src/pages/amd/components/StepTwo.vue
@@ -2,15 +2,17 @@
   <view class="container">
     <u-divider textSize="16" text="学生验证不扣钱，每年下单维持1年"></u-divider>
     <u-gap height="5"></u-gap>
+    <!-- src="https://article.biliimg.com/bfs/article/0c3ccfff19bfe7f9bb3e984378410a27ebe9c4bf.jpg" -->
     <img referrer="no-referrer|origin|unsafe-url" class="animate__animated animate__flipInX"
-      src="https://article.biliimg.com/bfs/article/0c3ccfff19bfe7f9bb3e984378410a27ebe9c4bf.jpg"
-      style="height: 520rpx;display:block;margin:0 auto;border: 2px solid #ff4c4c;border-radius: 10px;" />
+      src="https://article.biliimg.com/bfs/article/11bae270f1ae0184b246147011155450b71e04ae.jpg"
+      style="width: 100%;display:block;margin:0 auto;border: 2px solid #ff4c4c;border-radius: 10px;" />
     <view style="font-size: 36upx;color:red;font-weight: bolder;display: block;text-align: center;margin-top: 10upx;"
       class="animate__animated animate__bounce animate__repeat-3">
-      消失就成功<view style="display: inline-block;" class="animate__animated animate__bounce animate__infinite">👆</view>
-      扣费时间不变不显示为明年
+      <view style="display: inline-block;" class="animate__animated animate__bounce animate__infinite">👆</view>
+      消失就成功，且刷脸不扣钱
+
       <u-gap height="6"></u-gap>
-      <view>且验证刷脸不扣钱</view>
+      <view>扣费时间不变不显示为明年</view>
     </view>
 
     <!-- 底部按钮 -->
@@ -22,38 +24,39 @@
       <u-col span="12">
         <button @click="$u.debounce(clickStart, 1200, true)"
           class="ui-btn bg-blue round block shadow animate__animated animate__heartBeat animate__slower animate__infinite py-4">
-          下一步
+          开始验证（ Start Verify ）
         </button>
       </u-col>
     </u-row>
     <!-- <u-alert class=" animate__fadeInRight animate__slower" style="position: absolute;bottom: -80upx;right:0;"
-      :description="modalTitle" type="info"></u-alert> -->
-
+      :description="modalTitle" type="info">
+    </u-alert> -->
     <!-- 底部弹出层 -->
     <u-popup class="bg-stripes-grey" :round="10" :show="showPop" mode="bottom" @close="closePop" @open="showPop=true"
       closeable>
-      <view style="padding:30upx 20upx 20upx;font-size: 35upx;">
-        <view class="animate__animated animate__flipInX  p-3 animate__slow">
-          <text style="font-size: 40upx;">1、验证时，若提示</text>
-          <ui-tag class="animate__animated animate__hinge animate__slower animate__infinite" bg="bg-red-gradient"
+      <view style="font-size: 35upx;text-align: center;">
+        <view class="animate__flipInX p-3 animate__slow">
+          <text style="font-size: 30upx;">若提示：</text>
+          <ui-tag ui="sm" class="animate animate__heartBeat animate__slower animate__infinite" bg="bg-red-gradient"
             info="账户已使用" />
           <u-gap height="5"></u-gap>
-          <u-tag size="large" color="red" text="未成功，请截图【账户已使用】给客服解决" plain></u-tag>
+          <u-tag size="large" color="red" text="打开音乐>右上角头像>管理订阅，截图给客服" plain></u-tag>
 
           <!-- <view style="display: flex; justify-content: space-evenly;border-radius: 10upx;"
             class="mt-2 border border-blue p-2 ">
             <a href="javascript:;" @click="subscribePersonal">先开个人方案</a>
             <a href="javascript:;" @click="$u.debounce(jumpStart, 600, true)">再免费升级</a>
           </view> -->
+          <u-gap></u-gap>
         </view>
-        <view class="animate__animated p-3 animate__delay-1s animate__flipInX animate__slow">
+        <!-- <view class="animate__animated p-3 animate__delay-1s animate__flipInX animate__slow">
           <text style="font-size: 40upx;">2、若提示</text>
           <ui-tag class="animate__animated animate__flip animate__slower animate__infinite" bg="bg-red-gradient"
             info="我们无法验证您的身份" />
           <u-gap height="5"></u-gap>
           <u-tag size="large" color="red" text="换个浏览器验证，即可解决！如谷歌、qq浏览器等" plain></u-tag>
-        </view>
-        <view class="animate__animated animate__delay-2s animate__flipInX animate__slow">
+        </view> -->
+        <!-- <view class="animate__animated animate__delay-2s animate__flipInX animate__slow">
           <u-alert class=" animate__fadeInRight animate__slower" style="position: absolute;bottom: 70upx;right:0;"
             description="多截图、别问在吗、直接问" type="success"></u-alert>
           <u-gap height="20"></u-gap>
@@ -65,7 +68,7 @@
             </text><text class="pl-5">刷脸免费</text>
           </button>
         </view>
-        <u-gap></u-gap>
+        <u-gap></u-gap> -->
       </view>
     </u-popup>
   </view>
@@ -87,6 +90,7 @@
       }
     },
     created() {
+      this.startVerify()
       uni.$on('startVerify', () => {
         if (this.verifyAddr != null) {
           this.startVerify()
@@ -106,7 +110,9 @@
       },
       clickStart() {
         this.showPop = true
-        this.startVerify()
+        setTimeout(() => {
+          this.jumpStart()
+        }, 1500)
       },
       confirmStart() {
         if (!this.checkboxValue1[0]) {
@@ -180,7 +186,7 @@
     .btn {
       position: absolute;
       width: 100%;
-      bottom: 50upx;
+      bottom: 100upx;
       text-align: right;
     }
 
