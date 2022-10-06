50529e205337076ae95741ec3db483026b2d1812
hdtopku <1450948930@qq.com>
2022-10-06 18:14:50 +0800

    自动描述：2022-10-06-18:14:50

--- src/pages/amd/am.vue
+++ src/pages/amd/am.vue
@@ -1,5 +1,5 @@
 <template>
-  <view v-if="showPage" class="page">
+  <view v-if="showPage" class="page bg-pink-thin">
     <u-notify ref="uNotify"></u-notify>
     <view style="width: 88vw;">
       <!-- <u-steps :current="currentStep" iconPlacement="right">
